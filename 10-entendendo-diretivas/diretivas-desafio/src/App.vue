2d984010cfe6f448a1e90a6b2f8e0a2754cf928f
ETI - Douglas Rodrigues Ferreira Campos <drcampos@sescpe.com.br>
2023-02-06 10:41:44 -0300

    Diretivas - Methods e eventos - Crianção de diretivas personalidas

--- 10-entendendo-diretivas/diretivas-desafio/src/App.vue
+++ 10-entendendo-diretivas/diretivas-desafio/src/App.vue
@@ -4,32 +4,62 @@
 		<hr>
 		<!-- Exercício -->
 		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+		<button v-quando:click="acao">Executar</button>
+		<p v-quando:mouseenter="mouseEnter"
+		v-quando:mouseleave="mouseLeave">Teste mouse event!</p>
+
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives: {
+		quando: {
+			// Chamando um evento. 
+			// bind(el, binding) {
+			//	el.onclick = function (e) {
+			//		binding.value()
+			// }
+
+			// Outra forma de chama um evento.
+			bind(el, binding) {
+				const tipo = binding.arg
+				const fn = binding.value
+				el.addEventListener(tipo, fn)
+			}
+		}
+	},
+	methods: {
+		acao() {
+			alert('Ação Executada!')
+		},
+		mouseEnter() { // Methodo para quando o mouse passar por cima mostra no console
+			console.log('Mouse Enter!')
+		},
+		mouseLeave() {
+			console.log('Mouse Leave!')
+		}
+	},
 }
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+		font-size: 2.5rem;
+	}
 
-button {
-	margin: 10px 0px;
-	padding: 10px 20px;
-	font-size: 1.4rem;
-	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
-}
+	button {
+		margin: 10px 0px;
+		padding: 10px 20px;
+		font-size: 1.4rem;
+		border-radius: 5px;
+		color: #FFF;
+		background-color: #2196F3;
+	}
 </style>
