b648d54ca2d4dc03c61817a10ab459c6effead84
ETI - Douglas Rodrigues Ferreira Campos <drcampos@sescpe.com.br>
2023-03-10 15:41:55 -0300

    Projeto Trader - Utilizando vuetifyjs

--- projetos/03-acoes/stock-trader/src/store/modules/stocks.js
+++ projetos/03-acoes/stock-trader/src/store/modules/stocks.js
@@ -1,10 +1,19 @@
 export default {
-    state: {
+    state: { // State que vai ter uma lista de stocks varias.
         stocks: []
     },
-    mutations: {
+    mutations: { // Esse setStocks vai ser para alterar as ações.
         setStocks(state, stocks) {
-
+            state.stocks = stocks
+        }
+    },
+    actions: { // buyStock vai ser a compra de stocks.
+        buyStock({ commit }, order) {
+            commit()
+        },
+        // Iniciar os conjuntos que ações que eu tenho no projeto.
+        initStocks({ commit }) { // Inicializar a lista de ações.
+            commit('setStocks', stocks)
         }
     }
 }
